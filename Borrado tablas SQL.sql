3809ccba90639156558fa38a2a9373ecf09dc556
matrocha1814 <mwmrocha1814@gmail.com>
2024-05-19 19:08:00 -0300

    Add files via upload

---
+++ Borrado tablas SQL.sql
@@ -0,0 +1,15 @@
+DELETE FROM Usuarios WHERE id_usuario = 1;
+
+DELETE FROM Informes WHERE id_informe = 1;
+
+DELETE FROM MagistradosDenunciados WHERE id_magistrado = 1;
+
+DELETE FROM Denunciantes WHERE id_denunciante = 1;
+
+DELETE FROM Conjuez WHERE id_conjuez = 1;
+
+DELETE FROM ConjuezLegislador WHERE id_conjuez_legislador = 1;
+
+DELETE FROM DepartamentoJudicial WHERE id_departamento_judicial = 1;
+
+DELETE FROM alertas WHERE id_alertas = 1;
