e4e6b3ab3089751fea020ffe7626715ae952807b
matrocha1814 <mwmrocha1814@gmail.com>
2024-05-19 19:06:43 -0300

    Add files via upload

---
+++ Consulta tablas SQL.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Usuarios;
+
+SELECT * FROM Informes;
+
+SELECT * FROM MagistradosDenunciados;
+
+SELECT * FROM Denunciantes;
+
+SELECT * FROM Conjuez;
+
+SELECT * FROM ConjuezLegislador;
+
+SELECT * FROM DepartamentoJudicial;
+
+SELECT * FROM Alertas;
